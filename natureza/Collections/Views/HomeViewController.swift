90b81913e9f8a5e448c9a378fa24fdbeefc79d9d
schueda <a.schuedamenezes@gmail.com>
2021-11-22 09:47:05 -0300

    Tira o botão de abrir a camera. Coloca título pra view

--- natureza/Collections/Views/HomeViewController.swift
+++ natureza/Collections/Views/HomeViewController.swift
@@ -12,25 +12,6 @@ class HomeViewController: UIViewController {
     
     let viewModel = CollectionViewModel()
     
-    lazy var openCameraButton: UIButton = {
-        let button = UIButton()
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.setTitle("Open Camera", for: .normal)
-        button.setTitleColor(.purple, for: .normal)
-        button.backgroundColor = .lightGray
-        button.layer.cornerRadius = 30
-        button.titleLabel?.font = .systemFont(ofSize: 18)
-        button.addTarget(self, action: #selector(openCamera), for: .touchUpInside)
-        return button
-    }()
-    
-    
-    @objc func openCamera() {
-        
-        navigationController?.pushViewController(CameraViewController(), animated: false)
-        
-    }
-    
     lazy var homeCollectionView: HomeCollectionView = {
         let collectionView = HomeCollectionView()
         collectionView.translatesAutoresizingMaskIntoConstraints = false
@@ -39,25 +20,16 @@ class HomeViewController: UIViewController {
     
    override func viewDidLoad() {
         super.viewDidLoad()
+       
+       title = "Coleções"
+       navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.tintColor]
         
         setupHomeCollectionView()
-        setupOpenCameraButton()
         viewModel.retrieveImages()
    
   }
     
  // Funções para setar a tela
-    
-    private func setupOpenCameraButton() {
-        view.addSubview(openCameraButton)
-        openCameraButton.snp.makeConstraints { make in
-            make.height.equalTo(60)
-            make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom).offset(-32)
-            make.leading.equalTo(view.safeAreaLayoutGuide.snp.leading).offset(32)
-            make.trailing.equalTo(view.safeAreaLayoutGuide.snp.trailing).offset(-32)
-        }
-   }
-    
     private func setupHomeCollectionView() {
         view.addSubview(homeCollectionView)
         homeCollectionView.snp.makeConstraints { make in
