34a898ebd654b17e23d4f8b313b75a03c11c97af
schueda <a.schuedamenezes@gmail.com>
2021-11-24 14:02:25 -0300

    Corrige um conflito esquecido (kkkkk)

--- natureza/Camera/VMs/AfterShotViewModel.swift
+++ natureza/Camera/VMs/AfterShotViewModel.swift
@@ -5,66 +5,6 @@
 ////  Created by Gabriel Puppi on 16/11/21.
 ////
 //
-<<<<<<< HEAD
-//  AfterShotViewModel.swift
-//  natureza
-//
-//  Created by Gabriel Puppi on 16/11/21.
-//
-
-import Foundation
-import UIKit
-
-
-
-
-class AfterShotViewModel {
-    
-    
-    let fileManager = FileIOController()
-    
-    func createNewAlbumWithPhoto(asset: Data) {
-        
-        var collectionAsset = Collection(images: [])
-        collectionAsset.images.append(asset)
-        try? fileManager.write(collectionAsset, toDocumentNamed: "Natureza")
-        
-    }
-    
-    func addPhotoToAlbum(album: String){
-        
-    }
-    
-    func addNoteToPhoto() {
-        
-        
-        
-        
-    }
-    
-    
-    func savePhoto(image: UIImage){
-        
-        
-        let data = image.pngData()
-        
-        createNewAlbumWithPhoto(asset: data!)
-    }
-    
-    
-    
-    func getAlbum() {
-        
-        
-        
-        
-    }
-    
-    
-    
-    
-}
-=======
 //import Foundation
 //import UIKit
 //
@@ -110,4 +50,3 @@ class AfterShotViewModel {
 //
 //
 //}
->>>>>>> dev
